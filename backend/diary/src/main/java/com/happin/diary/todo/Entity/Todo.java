1e2fc24f627dc8d239fc90db2cae89ad85597e77
happ-in <gpqls304@naver.com>
2023-05-09 21:58:46 +0900

    Update: 수정 1. FE 기술 스택 Vue -> React 2. 목표만 세우고 시작도 안했던 개발 다시 시작

--- backend/diary/src/main/java/com/happin/diary/todo/Entity/Todo.java
+++ backend/diary/src/main/java/com/happin/diary/todo/Entity/Todo.java
@@ -1,2 +1,17 @@
-package com.happin.diary.todo.Entity;public class Todo {
+package com.happin.diary.todo.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Todo {
+
+    private LocalDateTime date;
+    private long sn;
+    private String todo;
 }
