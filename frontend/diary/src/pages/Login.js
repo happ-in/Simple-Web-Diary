3bc238e9953597aa064a89b61279c75d91bf63df
happ-in <gpqls304@naver.com>
2024-07-11 17:56:57 +0900

    Feat: Login 기본페이지 제작

---
+++ frontend/diary/src/pages/Login.js
@@ -0,0 +1,12 @@
+// import React, { useState, useEffect } from 'react'
+import kakaoLoginIcon from '../icons/kakao_login_large_wide.png'
+
+export const Login = () => {
+    return (
+        <div> 
+            <h1>Login</h1>
+            <img src={kakaoLoginIcon} alt="kakaoLoginIcon"></img>
+        </div>
+    )
+}
+export default Login;
